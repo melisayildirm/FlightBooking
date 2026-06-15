2b79721601e4ca18740abe795376a748d4fcf41b
Melisa <melisaaayildirm@gmail.com>
2026-06-15 22:38:40 +0300

    Implement header, navbar, slider and booking tag partial components for home page

---
+++ FlightBooking/Controllers/DeafultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Controllers
+{
+    public class DeafultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
