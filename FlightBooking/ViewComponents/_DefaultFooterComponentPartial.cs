653e2b3740a202740b7736a224532f114f50877d
Melisa <melisaaayildirm@gmail.com>
2026-06-18 22:49:06 +0300

    implement SkyAdmin admin dashboard header and interactive navigation components

---
+++ FlightBooking/ViewComponents/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents
+{
+    public class _DefaultFooterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
