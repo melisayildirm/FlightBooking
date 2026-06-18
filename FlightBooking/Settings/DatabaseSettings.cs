63991c1ae323aa75cd7873fc3780a28c3c77f981
Melisa <melisaaayildirm@gmail.com>
2026-06-19 02:13:04 +0300

    Implement MongoDB flight service layer and flight DTO mappings

---
+++ FlightBooking/Settings/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightBooking.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string FlightCollectionName { get; set; }
+      
+    }
+}
