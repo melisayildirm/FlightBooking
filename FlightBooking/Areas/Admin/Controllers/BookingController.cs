01fa3a09a6ad03014051b789a91eef92a587cde1
Melisa <melisaaayildirm@gmail.com>
2026-06-19 13:43:59 +0300

    add reservation action buttons to flight list items

--- FlightBooking/Areas/Admin/Controllers/BookingController.cs
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -9,5 +9,10 @@ namespace FlightBooking.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult BookingList()
+        {
+            return View();
+        }
     }
 }
